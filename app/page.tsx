1dcc24a53aa71470c490e96a6b78f7f8f2c6602e
anoopraju31 <anoopraju31@gmail.com>
2024-09-11 15:15:15 +0530

    ShadCN setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import { type FC } from 'react'
+import { Button } from '@/components/ui/button'
 
 const HomePage: FC = () => {
-	return <div>Home Page</div>
+	return (
+		<div>
+			Home Page
+			<Button>Click me</Button>
+		</div>
+	)
 }
 
 export default HomePage
