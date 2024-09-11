df0d8fd43f719b7ffbfc5ef7f09e728d4b136e49
anoopraju31 <anoopraju31@gmail.com>
2024-09-11 15:48:45 +0530

    Generate ethereum address

--- components/GenerateSeedPhrase/index.tsx
+++ components/GenerateSeedPhrase/index.tsx
@@ -20,21 +20,26 @@ const GenerateSeedPhrase: FC<Props> = ({ mnemonic, handleMnemonicChange }) => {
 	}
 
 	return (
-		<div className='flex items-center flex-col md:flex-row gap-4'>
-			<Input
-				type='text'
-				placeholder='Enter your mnemonic'
-				value={mnemonic}
-				onChange={handleChange}
-				className='border-gray-950'
-			/>
+		<div className='w-full p-4 flex flex-col gap-4 bg-gray-100 rounded-lg'>
+			<h2 className='text-xl font-semibold text-gray-950'>
+				Generate Seed Phrase
+			</h2>
+			<div className='flex items-center flex-col md:flex-row gap-4'>
+				<Input
+					type='text'
+					placeholder='Enter your mnemonic'
+					value={mnemonic}
+					onChange={handleChange}
+					className='border-gray-950'
+				/>
 
-			<Button
-				type='button'
-				className='bg-gray-800 hover:bg-gray-950/90 active:bg-gray-950/80'
-				onClick={generateMnemonics}>
-				Generate Mnemonic
-			</Button>
+				<Button
+					type='button'
+					className='bg-gray-800 hover:bg-gray-950/90 active:bg-gray-950/80'
+					onClick={generateMnemonics}>
+					Generate Mnemonic
+				</Button>
+			</div>
 		</div>
 	)
 }
